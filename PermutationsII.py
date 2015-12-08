bbab60964f8824001cea1911643b60f777200270
Tony-Chou <rabbit1314930@gmail.com>
2015-12-08 21:14:07 +0800

    Just change indent.
    N

--- PermutationsII.py
+++ PermutationsII.py
@@ -5,24 +5,23 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        l = []
-        ll = len(nums)
-        _buffer = [0]*ll
-        bbuffer = [False]*ll
-        return self.backtrack(nums, 0, ll, l, _buffer , bbuffer)
-    def backtrack(self, nums, i, ll, l, _buffer , bbuffer):
-        if  _buffer in l:
-            return
-        if i == ll:
-            l.append(_buffer[:])
-            return
-            
-        for n in xrange(ll):
-            if bbuffer[n]:
-                continue
-            else:
-                bbuffer[n] = True
-                _buffer[i] = nums[n]
-                self.backtrack(nums, i+1, ll, l, _buffer , bbuffer)
-                bbuffer[n] = False
-        return l
+        ans = []
+        num_len = len(nums)
+        _buffer = [0] * num_len
+        bbuffer = [False] * num_len
+        def backtrack(position):
+            if  _buffer in ans:
+                return
+            if position == num_len:
+                ans.append(_buffer[:])
+                return
+            for n in xrange(num_len):
+                if bbuffer[n]:
+                    continue
+                else:
+                    bbuffer[n] = True
+                    _buffer[position] = nums[n]
+                    backtrack(position+1)
+                    bbuffer[n] = False
+        backtrack(0)
+        return ans
